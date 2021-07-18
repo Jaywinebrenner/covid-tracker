d754191daad492ae38b6e681d047b6b581a29803
Jay Winebrenner <jaywinebrenner@gmail.com>
2021-07-17 20:41:26 -0700

    wire up state

--- src/App.js
+++ src/App.js
@@ -5,19 +5,24 @@ import React from 'react';
 import { Cards, Chart, CountryPicker} from "./components";
 import styles from './App.module.css';
 import { fetchData} from './api'
-import { render } from '@testing-library/react';
 
 class App extends React.Component {
+  state = {
+    data: {},
+  }
 
   async componentDidMount() {
-    const data = await fetchData();
-    console.log("data", data);
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData})
+    console.log("data", this.state.data);
   }
+
   render() {
+    const { data } = this.state;
     return (
       <div className={styles.container}>
         <h1>Apppp</h1>
-        <Cards/>
+        <Cards data={data}/>
         <Chart/>
         <CountryPicker/>
       </div>
